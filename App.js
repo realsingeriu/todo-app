be8130f8fdc3ca91842d8e287fe54733cc1cb389
realsingeriu <sda1613@gmail.com>
2024-02-05 09:55:07 +0900

    TextInput 텍스트 입력 설정

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
 import { StatusBar } from "expo-status-bar";
 import { theme } from "./color";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useState } from "react";
 
 export default function App() {
   const [working, setWorking] = useState(true);
   const travel = () => setWorking(false);
   const work = () => setWorking(true);
+  const [text, setText] = useState("");
+  const onChangText = (payload) => setText(payload);
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
@@ -29,6 +37,14 @@ export default function App() {
           </Text>
         </TouchableOpacity>
       </View>
+      <View>
+        <TextInput
+          onChangeText={onChangText}
+          value={text}
+          placeholder={working ? "할일 추가" : "어디로 여행 갈까요?"}
+          style={styles.input}
+        />
+      </View>
     </View>
   );
 }
@@ -48,5 +64,13 @@ const styles = StyleSheet.create({
     fontSize: 38,
     fontWeight: "600",
     color: "white",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+    marginTop: 20,
+    fontSize: 18,
   },
 });
